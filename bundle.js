ea98cb5f4ab561c195e7068b1aef5a7a3c25b03f
Oslo <Oslo.noh@bunjang.co.kr>
2025-03-20 10:33:58 +0900

    treeshake test

--- bundle.js
+++ bundle.js
@@ -12,8 +12,15 @@ function sum(a, b) {
 }
 
 //#endregion
+//#region src/lib.ts
+function used() {
+	console.log("이 함수는 사용됩니다");
+}
+
+//#endregion
 //#region src/main.js
 hello();
 sum(3, 4);
+used();
 
 //#endregion
